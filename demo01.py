50d7327d63ad6e76e757a5151d85764ba10a6fd3
wdk <1114548347@qq.com>
2021-11-09 17:50:07 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,28 @@
+# print("hello world！")
+# print("哈哈"+"嘻嘻") #字符串的拼接
+# print("哈哈"*100)
+# print(12,"haha",23.1)
+# """
+# 啦啦啦啦
+# """
+# a = input("请输入：")#默认输入的都是字符串格式
+# print("input获取的内容：",a)
+# #数据格式的转换
+# c = type(233)
+# print(c)
+# print(type(()))
+# print(type([]))
+
+
+# x = float(input("请输入："))
+# y = float(input("请输入："))
+# print("求和为：",x+y)
+
+a ="wqfugfuwfhwoafjwjs  whfoiawf"
+print(len(a))
+"""
+通过代码获取两段内容，并计算他们的长度的和。
+"""
+b = input("请输入:")
+c = input("请输入：")
+print("他们长度的和为：",len(b)+len(c))
